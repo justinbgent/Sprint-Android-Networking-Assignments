401b5ffadec2a3ad776bbf76988c697c63ff5f48
Justin Gent <justinbgent@gmail.com>
2019-09-03 16:21:47 -0600

    Create OceaniaCountry and OceaniaAPI files

--- M02/Assignment/app/src/main/java/com/example/oceaniacountries/MainActivity.kt
+++ M02/Assignment/app/src/main/java/com/example/oceaniacountries/MainActivity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+//    companion object{
+//        val country: List<OceaniaCountry>
+//    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
