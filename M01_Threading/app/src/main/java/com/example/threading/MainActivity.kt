9ac0e323b6293709be4319fba1ff11c1f8084303
Justin Gent <justinbgent@gmail.com>
2019-09-02 19:23:53 -0600

    Move MyAsyncTask class out of onCreate function put prime generator and progressBar visibility methods inside class. Create lateinit variable of MyAsyncTask thread for access in activity override methods.

--- M01_Threading/app/src/main/java/com/example/threading/MainActivity.kt
+++ M01_Threading/app/src/main/java/com/example/threading/MainActivity.kt
@@ -3,16 +3,13 @@ package com.example.threading
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+    inner class MyAsyncTask : AsyncTask<Unit, Int, String>() {
         fun primes(): Sequence<Long>{
             var i = 0L
             return sequence{
@@ -31,56 +28,56 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        class MyAsyncTask : AsyncTask<Unit, Int, String>() {
-            override fun onPreExecute() {
-                txt_primes.visibility = View.INVISIBLE
-                progressVisibility(true)
+        override fun onPreExecute() {
+            txt_primes.visibility = View.INVISIBLE
+            progressVisibility(true)
 
-                super.onPreExecute()
-            }
-
-            override fun doInBackground(vararg p0: Unit?): String {
-                val primeNumbers = primes().take(16000).joinToString(", ")
-                return "Primes: $primeNumbers"
-            }
-
-            override fun onPostExecute(result: String?) {
-                progressVisibility(false)
-                txt_primes.text = result
-                txt_primes.visibility = View.VISIBLE
-
-                super.onPostExecute(result)
-            }
-
-            override fun onCancelled() {
-                this.cancel(true)
-                super.onCancelled()
-            }
+            super.onPreExecute()
         }
 
+        override fun doInBackground(vararg p0: Unit?): String {
+            val primeNumbers = primes().take(16000).joinToString(", ")
+            return "Primes: $primeNumbers"
+        }
+
+        override fun onPostExecute(result: String?) {
+            progressVisibility(false)
+            txt_primes.text = result
+            txt_primes.visibility = View.VISIBLE
+
+            super.onPostExecute(result)
+        }
+
+        override fun onCancelled() {
+            this.cancel(true)
+            super.onCancelled()
+        }
+    }
+
+    lateinit var task: MyAsyncTask
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
 
-        var task = MyAsyncTask()
         btn_start_generating.setOnClickListener {
             task = MyAsyncTask()
             task.execute()
         }
         btn_cancel.setOnClickListener {
             txt_primes.visibility = View.VISIBLE
-            progressVisibility(false)
+            progressBar.visibility = View.INVISIBLE
             task.cancel(true)
         }
-
-
-
     }
 
     override fun onStop() {
-//        if(asyncTask != null){
-//            asyncTask.cancel(true)
-//        }
+        if(task != null){
+            task.cancel(true)
+            Log.i("Thread_canceled", "Yes")
+        }
+
         super.onStop()
     }
-
-
 }
