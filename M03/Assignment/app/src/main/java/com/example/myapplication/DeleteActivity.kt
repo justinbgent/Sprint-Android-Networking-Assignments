864e28e04b6b2d5b18235f6c99ba70505e3c49b3
Justin Gent <justinbgent@gmail.com>
2019-09-04 19:52:31 -0600

    Put and Delete button in MainActivity work

--- M03/Assignment/app/src/main/java/com/example/myapplication/DeleteActivity.kt
+++ M03/Assignment/app/src/main/java/com/example/myapplication/DeleteActivity.kt
@@ -6,6 +6,9 @@ import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.apapters.EverythingAdapter
+import com.example.myapplication.model.Employee
+import com.example.myapplication.retrofit.ApiInterface
+import com.example.myapplication.retrofit.Factory
 import kotlinx.android.synthetic.main.activity_delete.*
 import kotlinx.android.synthetic.main.activity_get.*
 import retrofit2.Call
@@ -25,5 +28,8 @@ class DeleteActivity : AppCompatActivity(), Callback<Void> {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_delete)
+
+        var apiInterface: ApiInterface = Factory.create()
+        apiInterface.deleteEmployeeById("1").enqueue(this)
     }
 }
--- M03/Assignment/app/src/main/java/com/example/myapplication/PutActivity.kt
+++ M03/Assignment/app/src/main/java/com/example/myapplication/PutActivity.kt
@@ -2,20 +2,41 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.apapters.EverythingAdapter
 import com.example.myapplication.model.Employee
+import com.example.myapplication.retrofit.ApiInterface
+import com.example.myapplication.retrofit.Factory
+import kotlinx.android.synthetic.main.activity_get.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.security.Principal
 
 class PutActivity : AppCompatActivity(), Callback<Employee> {
     override fun onFailure(call: Call<Employee>, t: Throwable) {
     }
 
     override fun onResponse(call: Call<Employee>, response: Response<Employee>) {
+        var item: List<Employee>? = listOf()
+        if (response.body() != null){
+            var nonNullEmployee: Employee = response.body() as Employee
+            item = listOf(nonNullEmployee)
+        }
+
+        recycler.setHasFixedSize(true)
+        val manager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        val adapter = EverythingAdapter(item)
+        recycler.layoutManager = manager
+        recycler.adapter = adapter
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_put)
+
+        var apiInterface: ApiInterface = Factory.create()
+        apiInterface.updateEmployee(Employee("Steve", 1, 25, "Principal Engineer")).enqueue(this)
     }
 }
