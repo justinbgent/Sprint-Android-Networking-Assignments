ca433bfa83ad683a80f96200592dcc7cb4b1d623
Justin Gent <justinbgent@gmail.com>
2019-09-04 19:44:58 -0600

    Post button in MainActivity works posting employee.

--- M03/Assignment/app/src/main/java/com/example/myapplication/PostActivity.kt
+++ M03/Assignment/app/src/main/java/com/example/myapplication/PostActivity.kt
@@ -2,7 +2,13 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.apapters.EverythingAdapter
 import com.example.myapplication.model.Employee
+import com.example.myapplication.retrofit.ApiInterface
+import com.example.myapplication.retrofit.Factory
+import kotlinx.android.synthetic.main.activity_get.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -12,10 +18,24 @@ class PostActivity : AppCompatActivity(), Callback<Employee> {
     }
 
     override fun onResponse(call: Call<Employee>, response: Response<Employee>) {
+        var item: List<Employee>? = listOf()
+        if (response.body() != null){
+            var nonNullEmployee: Employee = response.body() as Employee
+            item = listOf(nonNullEmployee)
+        }
+
+        recycler.setHasFixedSize(true)
+        val manager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        val adapter = EverythingAdapter(item)
+        recycler.layoutManager = manager
+        recycler.adapter = adapter
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post)
+
+        var apiInterface: ApiInterface = Factory.create()
+        apiInterface.addEmployee(Employee("David", 7, 30, "Intern")).enqueue(this)
     }
 }
