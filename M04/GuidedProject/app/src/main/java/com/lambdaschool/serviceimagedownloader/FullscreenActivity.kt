481500b44fc84273ba59991d17e32f26002112cb
Justin Gent <justinbgent@gmail.com>
2019-09-05 14:31:32 -0600

    Finish Services Guided Project

--- M04/GuidedProject/app/src/main/java/com/lambdaschool/serviceimagedownloader/FullscreenActivity.kt
+++ M04/GuidedProject/app/src/main/java/com/lambdaschool/serviceimagedownloader/FullscreenActivity.kt
@@ -4,15 +4,12 @@ import android.annotation.SuppressLint
 import android.app.DownloadManager
 import android.content.*
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
-import android.support.v4.app.AlarmManagerCompat
 import android.support.v4.content.LocalBroadcastManager
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_fullscreen.*
 
 /**
@@ -53,28 +50,92 @@ class FullscreenActivity : AppCompatActivity() {
         if (AUTO_HIDE) {
             delayedHide(AUTO_HIDE_DELAY_MILLIS)
         }
-        // TODO: S04M04-4 Start service
+
 
         false
     }
 
+    private lateinit var imageDownloadReceiver: BroadcastReceiver
+    private lateinit var downloadManager: DownloadManager
+    private lateinit var downloadReceiver: BroadcastReceiver
+    var enqueue: Long = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_fullscreen)
 
+        myImageDownload.setOnClickListener {
+            // TODO: S04M04-4 Start service
+            val serviceIntent = Intent(this, LargeImageDownloadService::class.java)
+            serviceIntent.putExtra(LargeImageDownloadService.BITMAP_WIDTH, fullscreen_content.width)
+            serviceIntent.putExtra(LargeImageDownloadService.BITMAP_HEIGHT, fullscreen_content.height)
+            startService(serviceIntent)
+//            myImageDownload.isEnabled = false
+        }
         // TODO: (Optional) S04M04-8 setupDownloadManager
+        downloadManager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+
+        downloadManagerButton.setOnClickListener {
+            val request = DownloadManager.Request(Uri.parse("https://i.imgur.com/HaSmgGn.jpg"))
+            enqueue = downloadManager.enqueue(request)
+        }
+
+        val downloadIntentFilter = IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE)
+        downloadReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                val action = intent?.action
+                when (action) {
+                    DownloadManager.ACTION_DOWNLOAD_COMPLETE -> {
+                        val query = DownloadManager.Query()
+                        query.setFilterById(enqueue)
+                        val c = downloadManager.query(query)
+                        if (c.moveToFirst()) {
+                            val columnIndex = c.getColumnIndex(DownloadManager.COLUMN_STATUS)
+                            if (DownloadManager.STATUS_SUCCESSFUL == c.getInt(columnIndex)) {
+                                val uriString = c.getString(c.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI))
+                                val uriFileDescriptor = contentResolver.openFileDescriptor(Uri.parse(uriString), "r")
+                                val width = fullscreen_content.width
+                                val height = fullscreen_content.height
+                                fullscreen_content.setImageBitmap(decodeSampledBitmapFileDescriptor(uriFileDescriptor, width, height));
+                            }
+                        }
+                    }
+                    DownloadManager.ACTION_NOTIFICATION_CLICKED -> {
+                    }
+                    DownloadManager.ACTION_VIEW_DOWNLOADS -> {
+                    }
+                }
+            }
+        }
+
+        registerReceiver(downloadReceiver, downloadIntentFilter)
+
+        showDownloadsButton.setOnClickListener {
+            val i = Intent()
+            i.action = DownloadManager.ACTION_VIEW_DOWNLOADS
+            startActivity(i)
+        }
 
         mVisible = true
         mControlsView = findViewById(R.id.fullscreen_content_controls)
         mContentView = findViewById(R.id.fullscreen_content)
 
         // TODO: S04M04-5 Create BroadcastReceiver
+        imageDownloadReceiver = object: BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                if (intent?.action == LargeImageDownloadService.FILE_DOWNLOADED){
+                    val bitmap = intent?.getParcelableExtra<Bitmap>(LargeImageDownloadService.DOWNLOADED_IMAGE)
+                    fullscreen_content.setImageBitmap(bitmap)
+                }
+            }
+        }
 
         // TODO: S04M04-6 Add IntentFilter
-
+        val intentFilter = IntentFilter().apply {
+            addAction(LargeImageDownloadService.FILE_DOWNLOADED)
+        }
         // TODO: S04M04-7 Register receiver
-
+        LocalBroadcastManager.getInstance(this).registerReceiver(imageDownloadReceiver, intentFilter)
         // Set up the user interaction to manually show or hide the system UI.
         mContentView?.setOnClickListener { toggle() }
 
@@ -158,6 +219,7 @@ class FullscreenActivity : AppCompatActivity() {
         override fun onDestroy() {
             super.onDestroy()
             // TODO S04M04-10 Unregister receivers
+            unregisterReceiver(imageDownloadReceiver)
         }
 
         fun showDownload(view: View) {
--- M04/GuidedProject/app/src/main/java/com/lambdaschool/serviceimagedownloader/LargeImageDownloadService.kt
+++ M04/GuidedProject/app/src/main/java/com/lambdaschool/serviceimagedownloader/LargeImageDownloadService.kt
@@ -1,7 +1,72 @@
 package com.lambdaschool.serviceimagedownloader
 
+import android.app.IntentService
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.support.v4.content.LocalBroadcastManager
+import android.util.Log
+import java.lang.UnsupportedOperationException
+
 // TODO: S04M04-1 create new service
+class LargeImageDownloadService: IntentService("LargeImageDownload"){
+    override fun onHandleIntent(intent: Intent?) {
+        Thread(Runnable() {
+            // TODO: S04M04-3 Add network call
+            val width = intent?.getIntExtra(BITMAP_WIDTH, 0) ?: 0
+            val height = intent?.getIntExtra(BITMAP_HEIGHT, 0) ?: 0
+            val bitmap = NetworkAdapter.getBitmapFromUrl("https://i.imgur.com/HaSmgGn.jpg", width, height)
+
+            val intent = Intent(FILE_DOWNLOADED).apply {
+                putExtra(DOWNLOADED_IMAGE, bitmap)
+            }
+            LocalBroadcastManager.getInstance(this).sendBroadcast(intent)
+
+        }).start()
+    }
+
+//    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+//        Log.i("CycleLife", "it StartCommand")
+//
+//        Thread(Runnable() {
+//            // TODO: S04M04-3 Add network call
+//            val width = intent?.getIntExtra(BITMAP_WIDTH, 0) ?: 0
+//            val height = intent?.getIntExtra(BITMAP_HEIGHT, 0) ?: 0
+//            val bitmap = NetworkAdapter.getBitmapFromUrl("https://i.imgur.com/HaSmgGn.jpg", width, height)
+//
+//            val intent = Intent(FILE_DOWNLOADED).apply {
+//                putExtra(DOWNLOADED_IMAGE, bitmap)
+//            }
+//            LocalBroadcastManager.getInstance(this).sendBroadcast(intent)
+//            stopSelf()
+//        }).start()
+//
+//
+//        return super.onStartCommand(intent, flags, startId)
+//    }
+//
+//    override fun onCreate() {
+//        Log.i("CycleLife", "it onCreate")
+//        super.onCreate()
+//    }
+//
+//    override fun onDestroy() {
+//        Log.i("CycleLife", "it Destroyed")
+//        super.onDestroy()
+//    }
+//
+//    override fun onBind(p0: Intent?): IBinder? {
+////        return null
+//        throw UnsupportedOperationException()
+//    }
+
+    companion object{
+        const val FILE_DOWNLOADED = "com.lambdashcool.serviceimagedownloader.FILE_DOWNLOADED"
+        const val DOWNLOADED_IMAGE = ""
+        const val BITMAP_HEIGHT = "HEIGHT"
+        const val BITMAP_WIDTH = "WIDTH"
+    }
+}
 
 // TODO: S04M04-2 Override onStartCommand
 
-// TODO: S04M04-3 Add network call
